65a9148da3c1306a2577e058695df62cccfadcf4
haoxin <cheng_hx@sohu.com>
2019-01-31 13:30:47 +0800

    First version

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textKey = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textExp = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textPhone = new System.Windows.Forms.TextBox();
+            this.returnLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1027, 641);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "发送消息";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textKey
+            // 
+            this.textKey.Location = new System.Drawing.Point(195, 101);
+            this.textKey.Name = "textKey";
+            this.textKey.Size = new System.Drawing.Size(531, 28);
+            this.textKey.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(102, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "秘钥";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "超期时间(小时)";
+            // 
+            // textExp
+            // 
+            this.textExp.Location = new System.Drawing.Point(195, 151);
+            this.textExp.Name = "textExp";
+            this.textExp.Size = new System.Drawing.Size(531, 28);
+            this.textExp.TabIndex = 4;
+            this.textExp.Text = "1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(83, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "手机号码";
+            // 
+            // textPhone
+            // 
+            this.textPhone.Location = new System.Drawing.Point(195, 213);
+            this.textPhone.Name = "textPhone";
+            this.textPhone.Size = new System.Drawing.Size(531, 28);
+            this.textPhone.TabIndex = 6;
+            this.textPhone.Text = "139";
+            // 
+            // returnLabel
+            // 
+            this.returnLabel.AutoSize = true;
+            this.returnLabel.Location = new System.Drawing.Point(101, 290);
+            this.returnLabel.Name = "returnLabel";
+            this.returnLabel.Size = new System.Drawing.Size(62, 18);
+            this.returnLabel.TabIndex = 7;
+            this.returnLabel.Text = "返回值";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(34, 653);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(934, 28);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 619);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 18);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "SAS Token";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1256, 757);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.returnLabel);
+            this.Controls.Add(this.textPhone);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textExp);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textKey);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "CEF 演示";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textExp;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textPhone;
+        private System.Windows.Forms.Label returnLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+using Newtonsoft.Json;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TimeSpan ts = new TimeSpan(System.Convert.ToInt32(textExp.Text) * 3600, 0, 0);
+            String SAS = CreateSASToken(textKey.Text, "full", ts);
+            textBox1.Text = SAS;
+            returnLabel.Text = sendSMS(SAS, textPhone.Text);
+        }
+        public static string CreateSASToken(string key, string keyName, TimeSpan timeout)
+        {
+            const string Schema = "SharedAccessSignature";
+            const string SignKey = "sig";
+            const string KeyNameKey = "skn";
+            const string ExpiryKey = "se";
+            
+            var values = new Dictionary<string, string>
+             {
+              { KeyNameKey, keyName },
+              { ExpiryKey, (DateTimeOffset.UtcNow + timeout).ToUnixTimeSeconds().ToString() }
+             };
+
+            var signContent = string.Join("&", values
+                .Where(pair => pair.Key != SignKey)
+                .OrderBy(pair => pair.Key)
+                .Select(pair => $"{pair.Key}={WebUtility.UrlEncode(pair.Value)}"));
+            string sign;
+            using (var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(key)))
+            {
+                sign = Convert.ToBase64String(hmac.ComputeHash(Encoding.UTF8.GetBytes(signContent)));
+            }
+
+            return $"{Schema} {SignKey}={WebUtility.UrlEncode(sign)}&{signContent}";
+        }
+
+        public static string sendSMS(string SASToken, String phoneNumber)
+        {
+          //HttpClient client = new HttpClient(new LoggingHandler(new HttpClientHandler()));
+          String url = "https://cef.chinacloudapi.cn/services/sms/messages?api-version=2018-10-01";
+          HttpClient client = new HttpClient();
+          client.DefaultRequestHeaders.Add("Account", "ceftest");
+          client.DefaultRequestHeaders.Add("Authorization", SASToken);
+          JObject message = new JObject(
+                new JProperty("phonenumber", 
+                    new JArray(phoneNumber)
+                  ),
+                new JProperty("extend", "09"),
+                new JProperty("messageBody",
+                   new JObject(
+                       new JProperty("templateName", "Notification1"),
+                       new JProperty("templateParam",
+                           new JObject(
+                              new JProperty("username", "testuser"),
+                              new JProperty("ticketnumber", "xxx0001")
+                         )))));
+            Console.WriteLine(message.ToString());
+            StringContent content = new StringContent(message.ToString(), Encoding.UTF8, "application/json");
+            var response = client.PostAsync(url, content).Result;
+            var body = response.Content.ReadAsStringAsync().Result;
+            return body.ToString();
+          
+        }
+
+
+    }
+    public class LoggingHandler : DelegatingHandler
+    {
+        public LoggingHandler(HttpMessageHandler innerHandler)
+            : base(innerHandler)
+        {
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Request:");
+            Console.WriteLine(request.ToString());
+            //request.Content = new StringContent("Hello world");
+            
+            if (request.Content != null)
+            {
+                Console.WriteLine(await request.Content.ReadAsStringAsync());
+            }
+            Console.WriteLine();
+            
+            HttpResponseMessage response = await base.SendAsync(request, cancellationToken);
+
+            Console.WriteLine("Response:");
+            Console.WriteLine(response.ToString());
+            if (response.Content != null)
+            {
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+            Console.WriteLine();
+            
+            return response;
+        }
+    }
+}
+
